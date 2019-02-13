c9fb37ea049f1daafcbbab58b7e14098793c9d6d
benjanamin <31413315+benjanamin@users.noreply.github.com>
2019-02-13 15:54:35 -0300

    cambios en html , js y css

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,25 @@
+var enviado=0;
 $("#enviar").click(function(){
-  alert("Hello! I am an alert box!!");
+  if(enviado==0){
+    alert("Nos pondremos en contacto contigo pronto.");
+    enviado=enviado+1
+  }
+  else{
+    alert("Ya has enviado tu mensaje, recarga si quieres mandar otro.");
+  }
+});
+
+$(function () {
+  $("li>a").click(function(event){
+    if(this.hash !=""){
+      event.preventDefault();
+
+      var desp=  this.hash;
+      $("html,body").animate({
+        scrollTop: $(desp).offset().top
+      },800,function(){
+        window.location.hash=desp;
+      });
+    }
+  });
 });
